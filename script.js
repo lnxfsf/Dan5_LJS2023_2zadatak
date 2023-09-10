1753c5130ef1807250ab16e1aa6cf57b672b9dcc
Linuxiness <igor.lerinc7@gmail.com>
2023-09-10 16:27:14 +0200

    dovrsen dizajn

--- script.js
+++ script.js
@@ -1,102 +1,155 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-	const charCountInput = document.getElementById("charCount");
-	const generateButton = document.getElementById("generate");
-	const addButton = document.getElementById("add");
-	const container = document.getElementById("container");
-	const pal = document.getElementById("pal");
+const charCountInput = document.getElementById("charCount");
+const napraviButton = document.getElementById("generisi");
+const dodajButton = document.getElementById("dodaj");
+const container = document.getElementById("container");
 
 
-	let squares = [];
-
-	generateButton.addEventListener("click", generateSquares);
-	addButton.addEventListener("click", addSquare);
+const pali = document.getElementById("pali");
 
 
-	function generateSquares() {
-		const charCount = parseInt(charCountInput.value);
+let squares = [];
 
-		if (isNaN(charCount) || charCount <= 0) {
-			alert("Molimo unesite validan broj karaktera.");
-			return;
-		}
+napraviButton.addEventListener("click", napraviKvadrate);
+dodajButton.addEventListener("click", dodajKvadrat);
 
-		squares = [];
-		container.innerHTML = "";
-		for (let i = 0; i < charCount; i++) {
-			createSquare();
-		}
+
+
+// na osnovu inputa sto se unese on ce ovde 
+function napraviKvadrate() {
+	const charCount = parseInt(charCountInput.value);
+
+
+	squares = [];
+	container.innerHTML = "";
+	for (let i = 0; i < charCount; i++) {
+		napraviKvadrat();
+	}
+}
+
+
+
+function napraviKvadrat(squareNumber) {
+
+
+
+	// on ovde na osnovu inputa napravi toliko 
+	const squareContainer = document.createElement("div"); // kreira div element
+	squareContainer.className = "square-container";  // u taj div element doda ovu klasu 
+
+
+	const square = document.createElement("div"); // kreira div element
+	square.className = "square"; // u taj div element doda ovu klasu 
+	square.contentEditable = true;  // da taj div moze u njega da unosimo tekst
+
+	square.style.backgroundColor = "white";
+	square.addEventListener("input", Palindrome);  // i kad se unese element, onda poziva funkciju da proveri da li je tekst palindrom
+
+	const numberSpan = document.createElement("span");  // kreira span element 
+	numberSpan.textContent = squareNumber;  // i assignuje broj div-a koji je. da bi znali da ga brisemo posle...
+
+
+
+// boxicon da se doda smece...
+const icon = document.createElement("i");
+icon.className = "bx bxs-trash-alt";
+	icon.style.backgroundColor = "white";
+
+
+	// ovo je delete button. dodaje ispod ovog gore. i stavlja event listener. a kao argument za brisanje se salje ovaj squareContainer sto sadrzi elemente i sve sto treba.. to je kao DOM referenca na taj div sto drzi ove buttons i jos jedan div zajedno...
+	const deleteButton = document.createElement("button");
+	deleteButton.className = "delete-button";
+	// da ima trash icon ovde
+	deleteButton.appendChild(icon);
+	deleteButton.style.backgroundColor = "white";
+	deleteButton.addEventListener("click", () => obrisiKvadrat(squareContainer));
+
+
+
+	// dodajemo u ovaj container, da bi ih sve prikazao u glavni..  div (container)
+	squareContainer.appendChild(numberSpan);
+	squareContainer.appendChild(square);
+	squareContainer.appendChild(deleteButton);
+
+	container.appendChild(squareContainer);
+	squares.push(square);
+}
+
+
+function dodajKvadrat() {
+	napraviKvadrat();
+}
+
+
+
+
+// on ovde obrise taj element. 
+function obrisiKvadrat(squareContainer) {
+
+
+
+
+	// trazi na koji indeks liste se nalazi ovaj element. jer je squareContainer referenca do tog izabranog div-a sto nam je... 
+	const index = squares.indexOf(squareContainer.querySelector(".square"));
+
+
+
+	// on ga sklanja iz liste
+	if (index !== -1) {
+		squares.splice(index, 1);
+	}
+
+	// da bi ga maknio i sa DOM-a..
+	container.removeChild(squareContainer);
+
+
+	// Ako nema vise kvadrata da makne onaj prikaz teksta za palindrom ili ne... 
+	if (squares.length === 0) {
+
+		// sakrij ovaj tekst za palindrom kada se sve obrise (kvadrati). OK, works
+		pali.style.display = "none";
+
+	}
+
+}
+
+function Palindrome() {
+
+
+
+	// on ovde proverava da li je palindrom, glavni deo
+	// samo A-Z , a-z i space moze da prodje
+	const text = this.innerText.toLowerCase().replace(/[^a-z ]/g, "");  // da očisti taj input string.. 
+
+	this.innerText = text;
+
+	// on ovde reversuje tekst, da bi uporedio reversed text i običan tekst koji je unešen
+	const reversedText = text.split("").reverse().join("");
+	const isPalindrome = text === reversedText; // to je ovde, da proveri da li normalan tekst odgovara sa reversed tekstom...
+
+
+
+
+	// promeni pozadinu kvadrata, i prikaze ispod message 
+	if (isPalindrome) {
+		this.style.backgroundColor = "#4CAF50";
+
+		pali.className = "pali";
+		pali.textContent = "Jeste palindrom";
+
+	} else {
+		this.style.backgroundColor = "#F44336";
+
+		pali.className = "pali";
+		pali.textContent = "Nije palindrom";
+
+
 	}
 
 
 
-	function createSquare(squareNumber) {
-		const squareContainer = document.createElement("div");
-		squareContainer.className = "square-container";
 
-		const square = document.createElement("div");
-		square.className = "square";
-		square.contentEditable = true;
-		square.addEventListener("input", checkPalindrome);
-
-		const numberSpan = document.createElement("span");
-		numberSpan.textContent = squareNumber;
-
-		const deleteButton = document.createElement("button");
-		deleteButton.className = "delete-button";
-		deleteButton.textContent = "X";
-		deleteButton.addEventListener("click", () => deleteSquare(squareContainer));
-
-		squareContainer.appendChild(numberSpan);
-		squareContainer.appendChild(square);
-		squareContainer.appendChild(deleteButton);
-
-		container.appendChild(squareContainer);
-		squares.push(square);
-	}
-
-
-	function addSquare() {
-		createSquare();
-	}
-
-	function deleteSquare(squareContainer) {
-		const index = squares.indexOf(squareContainer.querySelector(".square"));
-		if (index !== -1) {
-			squares.splice(index, 1);
-		}
-		container.removeChild(squareContainer);
-	}
-
-	function checkPalindrome() {
-		const text = this.innerText.toLowerCase().replace(/[^a-z ]/g, "");
-		this.innerText = text;
-		const reversedText = text.split("").reverse().join("");
-		const isPalindrome = text === reversedText;
-
-		if (isPalindrome) {
-			this.style.backgroundColor = "lightgreen";
-
-			pal.textContent = "Jeste palindrom";
-
-		} else {
-			this.style.backgroundColor = "red";
-			pal.textContent = "Nije palindrom";
-
-
-		}
-
-		const message = isPalindrome
-			? "Unijeta riječ je palindrom"
-			: "Unijeta riječ nije palindrom";
-		const messageElement = document.createElement("p");
-		messageElement.textContent = message;
-
-		const squareContainer = this.parentNode;
-		squareContainer.querySelector(".message").remove();
-		squareContainer.appendChild(messageElement);
-		messageElement.className = "message";
-	}
+}
 
 
 
@@ -104,7 +157,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-});
 
 
 
